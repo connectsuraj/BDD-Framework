92705f8057e9396d16a2c992455b88f3c142c2b1
connecsuraj <connect.suraj98@gmail.com>
2024-05-16 14:41:21 +0530

    Adding Folder to the project to handle the data

--- src/main/java/genericPackage/IAutoConstant.java
+++ src/main/java/genericPackage/IAutoConstant.java
@@ -5,28 +5,14 @@ public interface IAutoConstant {
 	String PROP_PATH = "./data/config.properties";
 	String CHROME_KEY = "webdriver.chrome.driver";
 	String CHROME_PATH = "./drivers/chromedriver.exe";
-	String SCREENSHOT_PATH = "./RPM Reports/TestCaseScreenShots/";
+	String SCREENSHOT_PATH = "C:\\Users\\Suraj Dalavi\\eclipse-workspace\\framework\\ScreenShots";
 	String EXCEL_PATH = "./data/TestData.xlsx";
 	String EXCEL = "./data/ClinicianData.xlsx";
 
 	// ADMIN>SETTINGS>---
 
-	String PORTAL_URL = "https://connectcareteam.com";
-	String PRIMARY_ADMIN_FIRST_NAME = "Alden";
-	String PRIMARY_ADMIN_LAST_NAME = "Stevens";
-	String PRIMARY_ADMIN_EMAIL = "alden.stevens+CCTTest1@ambiohealth.com";
-	String EMAIL_REPLY_EMAILADDRESS = "support@connectcareteam.com";
-	String LOGINPAGE_URL = "https://connectcareteam.com/account/login";
-	String REPORT_PATH = "C:\\Users\\Suraj Dalavi\\git\\repository\\com.ambiohealth.cct\\RPM Reports\\report.html";
+	String PORTAL_URL = "https://google.com";
+	String REPORT_PATH = "C:\\Users\\Suraj Dalavi\\eclipse-workspace\\framework\\Reports";
 
-//PAGE URLS	
-
-	String ADMIN_SETTINGS_FLAGPAGE_URL = "https://ambiohealth.com/companies/load/assessments/#/companies/settings/flags/";
-	String ADMIN_SETTINGS_ALERT_URL = "https://ambiohealth.com/companies/load/assessments/#/companies/settings/alerts/";
-	String ADMIN_DASHBOARD_URL = "https://ambiohealth.com/companies/load/caregiverdashboard";
-	String CLINICIAN1_DASHBOARD_URL = "https://ambiohealth.com/companies/load/assessments/#/companies/clinicians/cliniciandashboard/";
-	String PATIENT_DASHBOARD_SETTINGS_URL = "https://ambiohealth.com/load/patient-info/";
-	String PATIENT_HISTORY_PAGE_URL = "https://ambiohealth.com/companies/load/assessments/?history=1/#/companies/clinicians/clinicianpatientrecords/5591/";
-	String ADMIN_ANALYTICS_URL = "https://ambiohealth.com/companies/load/assessments/#/companies/analytics/csv/";
 
 }
